41547e7dbf4bb34b2134a901d90904b8788c0b0f
oF <sky0932039612@gmail.com>
2021-07-09 16:13:46 +0800

    新增回顧2

--- script/func.js
+++ script/func.js
@@ -12,15 +12,38 @@ console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
 });
-
+//隱藏內容
+function none(){
+  document.getElementById('history-web').style.display='none';
+}
 
 var title = document.getElementById('title');
+var logo = document.getElementById('logo');
+
+//更改圖示
+function c(bx){
+  logo.removeAttribute("class");
+  logo.classList.add("bx");
+  logo.classList.add(bx);
+  logo.classList.add("logo");
+}
+
 document.getElementById('home').onclick=function(){
+  none();
+  c('bx-home');
   document.title ='oF|Home';
   title.innerText='Home';
 }
 
-function about(){
+document.getElementById('about').onclick=function(){
   document.title ='oF|About';
-  title.innerText='About';
+  title.innerText='About Me';
+}
+
+document.getElementById('history').onclick=function(){
+  none();
+  document.title ='oF|History';
+  title.innerText='History';
+  c('bx-history');
+  document.getElementById('history-web').style.display='block';
 }
