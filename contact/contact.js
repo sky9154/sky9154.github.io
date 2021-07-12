b4ce067f5b08948b698387b6c334b5cb8ae4dcba
oF <sky0932039612@gmail.com>
2021-07-12 22:14:46 +0800

    建立基本contact

---
+++ contact/contact.js
@@ -0,0 +1,6 @@
+document.getElementById('discord').onclick=function(){
+    M.toast({html: 'DISCORD TAG： ᴋɪʀɪᴛᴏ#9998'})
+    const id='ᴋɪʀɪᴛᴏ#9998';
+    id.select();
+    document.execCommand("Copy");
+  }
