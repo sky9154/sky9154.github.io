932d6597637ea45b8fe3ac78e6ca05aff601ec1b
oF <sky0932039612@gmail.com>
2021-07-12 14:24:07 +0800

    完成首頁大綱

--- script/func.js
+++ script/func.js
@@ -17,14 +17,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
   });
-
-
-  
-$(document).ready(function(){
-    $('.slider').slider({
-      // 不顯示操控圓鈕
-      indicators: false,
-      // 每張照片播五秒
-      interval: 5000
-    })
-  })
---
+++ script/home.js
@@ -0,0 +1,76 @@
+//文字效果
+var textWrapper = document.querySelector('.home .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+.add({
+    targets: '.home .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+}).add({
+    targets: '.home',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
+function home(){
+    const homecard = [
+        {
+            title: "About Me",
+            image: "image/home.png",
+            link: "../about",
+            introduction: "關於我"
+        },
+        {
+            title: "Skills",
+            image: "image/skills.png",
+            link: "../skills",
+            introduction: "我學過的程式語言"
+        },
+        {
+            title: "Projects",
+            image: "image/projects.png",
+            link: "../projects",
+            introduction: "練習用的垃圾"
+        },
+        {
+            title: "Plugins",
+            image: "image/plugins.png",
+            link: "../plugins",
+            introduction: "實用的插件及模組"
+        },
+        {
+            title: "Explore",
+            image: "image/explore.png",
+            link: "../explore",
+            introduction: "好用的網站"
+        },
+        {
+            title: "Contact",
+            image: "image/contact.png",
+            link: "../contact",
+            introduction: "更多連結"
+        }
+    ];
+    const hcard = document.getElementById('home-card');
+    homecard.forEach((post) => {
+    hcard.innerHTML += `
+    <div class="col s12 m4">
+        <div class="card">
+            <a href="${post.link}">
+                <div class="card-image">
+                    <img src="${post.image}">
+                    <span class="card-title teal-text text-accent-3">${post.title}</span>
+                </div>
+            </a>
+            <div class="card-content">
+                <p>${post.introduction}</p>
+            </div>
+        </div>
+    </div>
+    `;
+    });
+};
