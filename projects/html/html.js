35abcf85a4bca92c547b32bd57b41366703c21e2
oF <sky0932039612@gmail.com>
2021-07-14 03:38:15 +0800

    set Projects/html

---
+++ projects/html/html.js
@@ -0,0 +1,28 @@
+// 文字效果
+var textWrapper = document.querySelector('.title .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+.add({
+    targets: '.title .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+}).add({
+    targets: '.title .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+delay: (el, i) => 150 + 25 * i
+}).add({
+    targets: '.title',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
