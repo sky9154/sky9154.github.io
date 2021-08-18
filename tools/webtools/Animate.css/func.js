b7ac47b6087f13af4500af01dc7482c7781fb89a
oF <sky0932039612@gmail.com>
2021-08-18 19:50:40 +0800

    刪除多於垃圾

--- tools/webtools/Animate.css/func.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('copy').onclick=function(){
-    M.toast({html: "已複製程式碼"});
-    var str = document.getElementById("copy");
-    window.getSelection().selectAllChildren(str);
-    document.execCommand('Copy')
-}
