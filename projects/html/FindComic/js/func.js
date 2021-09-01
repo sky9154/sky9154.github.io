bf3ddffa72e7b57845d3b397274adc659ba3725c
oF <sky0932039612@gmail.com>
2021-09-01 23:53:21 +0800

    新增動畫圖檔

--- projects/html/FindComic/js/func.js
+++ projects/html/FindComic/js/func.js
@@ -2,25 +2,45 @@
     const comic = [
         {
             title: "號碼查詢",
-            image: "876099490354987068/num.gif",
-            introduce: "輸入號碼以查詢相應的漫畫"
+            image: "882359036870271016/num.png",
+            introduce: "輸入號碼查詢相應的漫畫"
         },
         {
             title: "關鍵字查詢",
-            image: "876099974256001054/str.gif",
-            introduce: "輸入關鍵字以查詢相關的漫畫"
+            image: "882367311825240074/text.png",
+            introduce: "輸入關鍵字查詢相關的漫畫"
+        },
+        {
+            title: "本日排行",
+            image: "882549985743372298/now.png",
+            introduce: "查詢本日熱門漫畫"
+        },
+        {
+            title: "本周排行",
+            image: "882549993205010432/week.png",
+            introduce: "查詢本周熱門漫畫"
+        },
+        {
+            title: "綜合排行",
+            image: "882550003074215976/all.png",
+            introduce: "查詢最熱門漫畫"
+        },
+        {
+            title: "隨機",
+            image: "882550020501544980/random.png",
+            introduce: "隨機推薦漫畫"
         }
     ];
     const demo = document.getElementById("demo");
     comic.forEach(post => {
     demo.innerHTML += `
-    <div class="col s12 m6">
+    <div class="col s12 m6 xl4">
         <div class="card">
             <div class="card-image">
-                <img src="https://media.discordapp.net/attachments/873610724470046761/${post.image}">
-                <span class="card-title black-text">${post.title}</span>
+                <img src="https://cdn.discordapp.com/attachments/882358958189318144/${post.image}">
             </div>
             <div class="card-content">
+                <p class="demo-title">${post.title}</p>
                 <p>${post.introduce}</p>
             </div>
         </div>
