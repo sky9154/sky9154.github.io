61b7e57942a599913e39ca2a1be2fe4d47d9fa1e
oF <sky0932039612@gmail.com>
2021-09-02 20:05:50 +0800

    建立 demo

--- projects/html/FindComic/js/func.js
+++ projects/html/FindComic/js/func.js
@@ -2,47 +2,43 @@
     const comic = [
         {
             title: "號碼查詢",
-            image: "882359036870271016/num.png",
+            image: "882672276917608488/number.png",
             introduce: "輸入號碼查詢相應的漫畫"
         },
         {
             title: "關鍵字查詢",
-            image: "882367311825240074/text.png",
+            image: "882672284505104474/text.png",
             introduce: "輸入關鍵字查詢相關的漫畫"
         },
         {
             title: "本日排行",
-            image: "882549985743372298/now.png",
+            image: "882672325223411712/day.png",
             introduce: "查詢本日熱門漫畫"
         },
         {
             title: "本周排行",
-            image: "882549993205010432/week.png",
+            image: "882672343799975936/week.png",
             introduce: "查詢本周熱門漫畫"
         },
         {
             title: "綜合排行",
-            image: "882550003074215976/all.png",
+            image: "882672385650724874/all.png",
             introduce: "查詢最熱門漫畫"
         },
         {
             title: "隨機",
-            image: "882550020501544980/random.png",
+            image: "882672448938606662/random.png",
             introduce: "隨機推薦漫畫"
         }
     ];
     const demo = document.getElementById("demo");
     comic.forEach(post => {
     demo.innerHTML += `
-    <div class="col s12 m6 xl4">
-        <div class="card">
-            <div class="card-image">
-                <img src="https://cdn.discordapp.com/attachments/882358958189318144/${post.image}">
-            </div>
-            <div class="card-content">
-                <p class="demo-title">${post.title}</p>
-                <p>${post.introduce}</p>
-            </div>
+    <div class="col s12 demo">
+        <img src="https://cdn.discordapp.com/attachments/882672248417304616/${post.image}" class="col s12 m6">
+        <div class="col s12 m6">
+            <h5>${post.title}</h5>
+            <p>${post.introduce}</p>
         </div>
     </div>
     `;
