016add95733ad7368db4e058282989e3f12cbff9
oF <sky0932039612@gmail.com>
2021-09-09 02:03:37 +0800

    新增 Button

--- assets/js/layout/contact.js
+++ assets/js/layout/contact.js
@@ -1,1 +1,50 @@
+// Button
+(() => {
+    const Button = [
+        {
+            name: "FaceBook",
+            icon: "ai-facebook-fill",
+            link: "www.facebook.com/weii.0504/"
+        },
+        {
+            name: "Instagram",
+            icon: "ai-instagram-fill",
+            link: "www.instagram.com/weii_0504/"
+        },
+        {
+            name: "Twitter",
+            icon: "ai-twitter-fill",
+            link: "www.twitter.com/Magus_0504"
+        },
+        {
+            name: "Bahamut",
+            icon: "ai-game-controller",
+            link: "home.gamer.com.tw/homeindex.php?owner=evan9154"
+        },
+        {
+            name: "Github",
+            icon: "ai-github-fill",
+            link: "github.com/sky0966548546"
+        },
+        {
+            name: "Discord",
+            icon: "ai-discord-fill",
+            link: "github.com/sky0966548546"
+        },
+    ];
+    const link = document.getElementById("link");
+    Button.forEach(post => {
+    link.innerHTML += `
+    <a href="https://${post.link}" target="_blank">
+        <div class="col s6 m4 icon">
+            <div class="card-panel center-align" id="${post.name}">
+                <span><i class="${post.icon}"></i> ${post.name}</span>
+            </div>
+        </div>
+    </a>
+    `;
+    });
+})();
+
+// plate initialization
 $(".icon").plate({inverse: true});
