9083d9ea427c11662e64f375a68612bb432420ce
sky9154 <sky0932039612@gmail.com>
2024-05-05 20:50:39 +0800

    v2.1

--- src/components/Project/ProjectList.tsx
+++ src/components/Project/ProjectList.tsx
@@ -3,14 +3,14 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { FaShareNodes } from 'react-icons/fa6';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import { H2, Paragraph } from '@components/Typography';
+import { H1, H2 } from '@components/Typography';
 import { useThemeModeContext } from '@context/ThemeModeContext';
 import { DarkTheme, LightTheme } from '@assets/theme';
 import { project, ProjectType } from '@assets/data/project';
+import More from '@assets/images/Project/More.webp';
 
 
 interface Props {
@@ -23,6 +23,13 @@ const ProjectList: FC<Props> = ({ page, type }) => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('md'));
   const [projectList, setProjectList] = useState<ProjectType[]>(project);
+  const [projectCardColor, setProjectCardColor] = useState<{
+    color: string;
+    solidColor: string;
+  }>({
+    color: '',
+    solidColor: ''
+  });
 
   useEffect(() => {
     const newPage = (matches) ? page : 0;
@@ -31,12 +38,23 @@ const ProjectList: FC<Props> = ({ page, type }) => {
     if (newProjectList.length !== 4 || !matches) {
       newProjectList.push({
         name: 'View More !',
+        cover: More,
         link: 'https://github.com/sky9154'
       });
     }
 
     setProjectList(newProjectList);
   }, [page, matches]);
+
+  useEffect(() => {
+    const color = (themeMode === 'dark') ? DarkTheme.ProjectCardColor : LightTheme.ProjectCardColor;
+    const solidColor = color.slice(0, -2);
+
+    setProjectCardColor({
+      color: color,
+      solidColor: solidColor
+    });
+  }, [themeMode]);
 
   return (
     <>
@@ -46,9 +64,6 @@ const ProjectList: FC<Props> = ({ page, type }) => {
             minHeight: '40vh',
             width: '100%',
             justifyContent: 'center',
-            bgcolor: (themeMode === 'dark') ? DarkTheme.InfoCardColor : LightTheme.InfoCardColor,
-            backdropFilter: 'blur(8px)',
-            borderRadius: 5,
             m: 2
           }}>
             <AnimatePresence mode="wait">
@@ -63,32 +78,40 @@ const ProjectList: FC<Props> = ({ page, type }) => {
                 whileInView="visible"
                 initial="hidden"
                 viewport={{ once: true }}
+                style={{ height: '100%' }}
               >
-                <Stack
-                  direction="column"
-                  spacing={4}
-                  width="100%"
-                  height="100%"
-                  py={4}
-                  px={2}
-                >
-                  <Box sx={{
+                <Box sx={{
+                  display: 'flex',
+                  alignItems: 'flex-end',
+                  height: '100%',
+                  width: '100%',
+                  p: 2,
+                  position: 'relative',
+                  backgroundImage: `url(${item.cover})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center top',
+                  backdropFilter: 'blur(8px)',
+                  borderImage: `fill 0 linear-gradient(${projectCardColor.color}, ${projectCardColor.solidColor})`,
+                  borderRadius: 5,
+                  clipPath: 'inset(0 round 20px)'
+                }}>
+                  <H2>{item.name}</H2>
+                  <H1 sx={{
                     display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    height: { ms: 'auto', md: '50px' }
+                    justifyContent: 'flex-end',
+                    alignItems: 'flex-start',
+                    height: '100%',
+                    padding: '32px 24px',
+                    position: 'absolute',
+                    right: 0
                   }}>
-                    <H2>{item.name}</H2>
-                    <H2>
-                      <IconButton target='_blank' href={item.link as string}>
-                        <FaShareNodes style={{
-                          color: (themeMode === 'dark') ? DarkTheme.FontColor : LightTheme.FontColor
-                        }} />
-                      </IconButton>
-                    </H2>
-                  </Box>
-                  <Paragraph sx={{ fontSize: '16px' }}>{item.about}</Paragraph>
-                </Stack>
+                    <IconButton target='_blank' href={item.link as string}>
+                      <FaShareNodes style={{
+                        color: (themeMode === 'dark') ? DarkTheme.FontColor : LightTheme.FontColor
+                      }} />
+                    </IconButton>
+                  </H1>
+                </Box>
               </motion.div>
             </AnimatePresence>
           </Grid>
